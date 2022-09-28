2bd7b55d31c4d2c2d0ff8d3ee51400a95fb0baf1
Samuel Vijaykumar M <mad.vijay@gmail.com>
2022-09-28 10:51:49 +0530

    Just a comment

--- zig/wasmtime/fib.py
+++ zig/wasmtime/fib.py
@@ -9,4 +9,5 @@ wasi_env = wasi.StateBuilder('fibonacci_number').finalize()
 import_obj = wasi_env.generate_import_object(store, wasi_version)
 instance = Instance(module, import_obj)
 # This should return 55
+# 0,1,1,2,3,5,8,13,21,34,55
 print("{0}".format(instance.exports.fibonacci(10)))
