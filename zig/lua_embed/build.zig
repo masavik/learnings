c5e46b90645491c4b50068ef3e6bea1babf28758
Samuel Vijaykumar <mad.vijay@gmail.com>
2022-10-04 09:26:42 +0530

    lua embed DONE

--- zig/lua_embed/build.zig
+++ zig/lua_embed/build.zig
@@ -1,63 +1,75 @@
-const exe = b.addExecutable("wrapper", "wrapper.zig");
-exe.setTarget(target);
-exe.setBuildMode(mode);
-exe.linkLibC();
-exe.addIncludeDir("lua-5.4.4/src");
+const std = @import("std");
+var builder: *std.build.Builder = undefined;
 
-const lua_c_files = [_][]const u8{
-    "lapi.c",
-    "lauxlib.c",
-    "lbaselib.c",
-    "lcode.c",
-    "lcorolib.c",
-    "lctype.c",
-    "ldblib.c",
-    "ldebug.c",
-    "ldo.c",
-    "ldump.c",
-    "lfunc.c",
-    "lgc.c",
-    "linit.c",
-    "liolib.c",
-    "llex.c",
-    "lmathlib.c",
-    "lmem.c",
-    "loadlib.c",
-    "lobject.c",
-    "lopcodes.c",
-    "loslib.c",
-    "lparser.c",
-    "lstate.c",
-    "lstring.c",
-    "lstrlib.c",
-    "ltable.c",
-    "ltablib.c",
-    "ltm.c",
-    "lua.c",
-    "luac.c",
-    "lundump.c",
-    "lutf8lib.c",
-    "lvm.c",
-    "lzio.c",
-};
+pub fn build(b: *std.build.Builder) void {
+    builder = b;
 
-if(target.os_tag == std.Target.Os.Tag.windows) {
-    const c_flags = [_][]const u8{
-        "-std=c99",
-        "-O2",
-        "-DLUA_USE_WINDOWS"
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+
+    const exe = b.addExecutable("wrapper", "wrapper.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.linkLibC();
+    exe.addIncludeDir("lua-5.4.4/src");
+
+    const lua_c_files = [_][]const u8{
+        "lapi.c",
+        "lauxlib.c",
+        "lbaselib.c",
+        "lcode.c",
+        "lcorolib.c",
+        "lctype.c",
+        "ldblib.c",
+        "ldebug.c",
+        "ldo.c",
+        "ldump.c",
+        "lfunc.c",
+        "lgc.c",
+        "linit.c",
+        "liolib.c",
+        "llex.c",
+        "lmathlib.c",
+        "lmem.c",
+        "loadlib.c",
+        "lobject.c",
+        "lopcodes.c",
+        "loslib.c",
+        "lparser.c",
+        "lstate.c",
+        "lstring.c",
+        "lstrlib.c",
+        "ltable.c",
+        "ltablib.c",
+        "ltm.c",
+        "lundump.c",
+        "lutf8lib.c",
+        "lvm.c",
+        "lzio.c",
     };
-    inline for (lua_c_files) |c_file| {
-        exe.addCSourceFile("lua-5.4.4/src/" ++ c_file, &c_flags);
+
+    if (target.os_tag == std.Target.Os.Tag.windows) {
+        const c_flags = [_][]const u8{ "-std=c99", "-O2", "-DLUA_USE_WINDOWS" };
+        inline for (lua_c_files) |c_file| {
+            exe.addCSourceFile("lua-5.4.4/src/" ++ c_file, &c_flags);
+        }
+    } else {
+        const c_flags = [_][]const u8{
+            "-std=c99",
+            "-O2",
+            "-DLUA_USE_POSIX",
+        };
+        inline for (lua_c_files) |c_file| {
+            exe.addCSourceFile("lua-5.4.4/src/" ++ c_file, &c_flags);
+        }
     }
-} else {
-    const c_flags = [_][]const u8{
-        "-std=c99",
-        "-O2",
-        "-DLUA_USE_POSIX",
-    };
-    inline for (lua_c_files) |c_file| {
-        exe.addCSourceFile("lua-5.4.4/src/" ++ c_file, &c_flags);
+    exe.install();
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
     }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
-exe.install();
--- zig/lua_embed/wrapper.zig
+++ zig/lua_embed/wrapper.zig
@@ -1,19 +1,33 @@
 // https://cone.codes/posts/using-zig-to-build-native-lua-scripts/
 const std = @import("std");
 
+pub const LUA_BYTECODE = @embedFile("luac.out");
+
 const lua = @cImport({
     @cInclude("lua.h");
     @cInclude("lualib.h");
     @cInclude("lauxlib.h");
-})
-
-pub const LUA_BYTECODE = @embedFile("luac.out");
+});
 
 pub fn main() anyerror!void {
+    // Creates a new Lua state. It calls lua_newstate with an allocator based on the standard C realloc function
+    // and then sets a panic function (see §4.6) that prints an error message to the standard error output in case
+    // of fatal errors
     var s = lua.luaL_newstate();
+    // Opens all standard Lua libraries into the given state.
     lua.luaL_openlibs(s);
-    const load_status = lua_luaL_loadbufferx(s, LUA_BYTECODE, LUA_BYTECODE.len, "main.lua", "bt");
+    // Loads a buffer as a Lua chunk. This function uses lua_load to load the chunk in the buffer pointed
+    // to by buff with size sz.
+    const load_status = lua.luaL_loadbufferx(s, LUA_BYTECODE, LUA_BYTECODE.len, "main.lua", "bt");
     if (load_status != 0) {
         std.log.info("Couldn't load lua bytecode: {s}", .{lua.lua_tolstring(s, -1, null)});
+        return;
+    }
+    // This function behaves exactly like lua_pcall, but allows the called function to yield
+    // lua_pcall, Calls a function in protected mode.
+    const call_status = lua.lua_pcallk(s, 0, lua.LUA_MULTRET, 0, 0, null);
+    if (call_status != 0) {
+        std.log.info("{s}", .{lua.lua_tolstring(s, -1, null)});
+        return;
     }
 }
