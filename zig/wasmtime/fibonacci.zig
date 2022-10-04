8f57bd784c46e7cab82b0887f8ed2ac5fd1a5591
Samuel Vijaykumar <mad.vijay@gmail.com>
2022-10-04 12:23:13 +0530

    wasm trials DONE

--- zig/wasmtime/fibonacci.zig
+++ zig/wasmtime/fibonacci.zig
@@ -1,4 +1,10 @@
+const print = @import("std").debug.print;
+
 export fn fibonacci(n: u32) u32 {
     if (n == 0 or n == 1) return n;
     return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+pub fn main() void {
+    print("{d}\n", .{fibonacci(10)});
 }
